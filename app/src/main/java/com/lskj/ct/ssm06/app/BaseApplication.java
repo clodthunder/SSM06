aa0f3174a8710b0bffb1c56ea74bb5119972fd04
thunder <tzwjason@163.com>
2017-11-19 17:21:27 +0800

    上次提交补充

--- app/src/main/java/com/lskj/ct/ssm06/app/BaseApplication.java
+++ app/src/main/java/com/lskj/ct/ssm06/app/BaseApplication.java
@@ -1,11 +1,24 @@
 package com.lskj.ct.ssm06.app;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 /**
  * Created by Json on 2017/11/16.
  */
 
-public class MyApp extends Application {
+public class BaseApplication extends Application {
 
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
 }
